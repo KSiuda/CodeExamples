9da139c23ade50892256e7658144d2130b5f7bd1
KSiuda <konrad.siuda@gmail.com>
2023-03-08 10:12:20 +0100

    Add project files.

---
+++ LINQExp/LINQExp/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace LINQExp
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			//LINQ - Language integrated query
+
+			// The Three Parts of a LINQ Query:
+			// 1. Data source.
+			var numbers = new[] { 0, 1, 2, 3, 4, 5, 6 };
+			var ship = new Ship();
+			var ships = new List<Ship> { };
+
+			// 2. Query creation.
+			// numQuery is an IEnumerable<int>
+			var numQueryOld =
+				from num in numbers
+				where (num % 2) == 0
+				select num;
+
+			var numList = numQueryOld.ToList();
+
+			var numArrayNew = numbers.Where(num => num % 2 == 0).Select(num => num).ToArray();
+
+			// 3. Query execution.
+			foreach (var num in numList)
+			{
+				Console.WriteLine($"{num}");
+			}
+
+			numList.ForEach(num =>
+			{
+				if (num > 2)
+				{
+
+				}
+			});
+
+			//static
+			MathHelper.Multiply(5, 4);
+			Math.Pow(5, 100);
+			Console.WriteLine();
+
+			//non static
+			var a = new Ship();
+			a.SetCoolNumber(5);
+		}
+
+		
+	}
+}
---
+++ LINQExp/LINQExp/Ship.cs
@@ -0,0 +1,31 @@
+﻿namespace LINQExp
+{
+	public class Ship
+	{
+		//1. Automatic property
+		public int Id { get; set; }
+
+		//2. Get only property
+		public int Count => list.Count;
+		//{ get{ return list.Count }
+		
+		//3. Init property
+		public List<int> list { get; init; } = new();
+
+		//4. Set/init only property
+		public Guid UniqueIdentifier
+		{
+			init => UniqueIdentifier = new Guid();
+		}
+
+		//5. Private set property
+		public int CoolNumber { get; private set; }
+
+		public bool SetCoolNumber(int coolNumber)
+		{
+			coolNumber = coolNumber * 2;
+			CoolNumber = coolNumber;
+			return true;
+		}
+	}
+}
